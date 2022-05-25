bb503928e8bf04b5b227e386ffb0b7b3667d8cfd
davlago <davlago@ucm.es>
2022-05-25 10:12:32 +0200

    add

---
+++ experiencia.php
@@ -0,0 +1,26 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale =1">
+		<link rel = "stylesheet" href = "style.css">
+		<title background-color: #DDE1EF; >Crea tu curriculum</title>
+
+	</head>
+	<body>
+		<div class="form-title">
+			<h1>CREA TU CV ONLINE</h1>
+		</div>
+		<div class="form-main">
+			<h2>EXPERIENCIAS</h2>
+			<h3>Presentación:</h3>
+			<input class ="parrafosTexto" type = "text" id="name" name ="name" placeholder="Nombre">
+			<h3>Experiencia:</h3>
+			<input class ="datosTexto" type = "text" id="lastName1" name ="lastName1" placeholder="Primer apellido">
+            <h3>Aptitudes:</h3>
+			<input class ="datosTexto" type = "text" id="lastName2" name ="lastName2" placeholder="Segundo apellido">
+			<h3>Idiomas:</h3>
+			<input class ="datosTexto" type = "email" id="mail" name ="mail" placeholder="Email">
+			<input class ="botons" type = "button" onClick="location.href='experiencia.html'"value ="Siguiente Paso">
+		</div>
+	</body>
+</html>
